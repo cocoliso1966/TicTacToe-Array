f11c10f4086a0736c8ad242ab716144e1c7df22e
Sebastian Asuncion Montero <desarrollo.espinacas1966@gmail.com>
2023-04-13 20:24:24 +0200

    8 commit. Desarrollo minimax. 13/04/2023

--- src/com/engendro/TicTacToe/IA.java
+++ src/com/engendro/TicTacToe/IA.java
@@ -1,16 +1,20 @@
 package com.engendro.TicTacToe;
 
 public class IA {
-	//  DEFINICION DE ELEMENTOS DE LA CLASE IA.
+//  DEFINICION DE ELEMENTOS DE LA CLASE IA.
 	private String[][] arbol = new String[5][5];
 	private int fila ;    		// ladoA
 	private int columna;  		// ladoB
 	private String player;		// Humano
 	private String opponent;	//Ordenador
 	
-	//  CONSTRUCTOR DE LA CLASE IA.
+//  CONSTRUCTOR DE LA CLASE IA.
 	public IA() { }
 	
+	// Este constructor completo requiere los siguientes parametros:
+	// gamer: El tablero completo del juego total que se copia en arbol.
+	// fila, columna: Las dimensiones del tablero del juego.
+	// humano, ordenador: Extraemos el elemento nombre de ambos jugadores.
 	public IA(Tablero gamer, int fila, int columna, Jugador humano, Jugador ordenador) {
 		this.fila = fila;
 		this.columna = columna;
@@ -47,43 +51,88 @@ public class IA {
 	
 //  METODOS, PROCEDIMIENTOS  Y FUNCIONES.
 	
-	public int evaluate(int profundidad) {
+	// Función para evaluar el resultado de una jugada realizada en arbol.
+	// El parámetro barbol[][], es una copia de arbol de un momento del juego que evalua
+	// en la función minimax. 
+	// Cada invocación recursiva de minimax tendra un arbol distinto.
+	// 
+	public int evaluate(String barbol[][]) {
 		
 		// Verificar las filas.
 		for (int row = 0; row < fila; row++) 
 		{ 
-			if (arbol[row][0] == arbol[row][1] && arbol[row][1] == arbol[row][2])
+			if (barbol[row][0] == barbol[row][1] && barbol[row][1] == barbol[row][2])
 			{
-				if (arbol[row][0] == player) return +10;
-				else if (arbol[row][0] == opponent) return -10;
+				if (barbol[row][0] == player) return +10;
+				else if (barbol[row][0] == opponent) return -10;
 			}
 		}
 		
 		// Verificar las columnas.
 		for (int col = 0; col < columna; col++)
 		{
-			if (arbol[0][col] == arbol[1][col] && arbol[1][col] == arbol[2][col])
+			if (barbol[0][col] == barbol[1][col] && barbol[1][col] == barbol[2][col])
 			{
-				if (arbol[0][col] == player) return +10;
-				else if (arbol[0][col] == opponent) return -10;
+				if (barbol[0][col] == player) return +10;
+				else if (barbol[0][col] == opponent) return -10;
 			}
 		}
 		
 		// Verificar las diagonales.
-		if (arbol[0][0] == arbol[1][1] && arbol[1][1] == arbol[2][2])
+		if (barbol[0][0] == barbol[1][1] && barbol[1][1] == barbol[2][2])
 		{
-			if (arbol[0][0] == player) return +10;
-			else if (arbol[0][0] == opponent) return -10;
+			if (barbol[0][0] == player) return +10;
+			else if (barbol[0][0] == opponent) return -10;
 		}
 		
-		if (arbol[0][2] == arbol[1][1] && arbol[1][1] == arbol[2][0])
+		if (barbol[0][2] == barbol[1][1] && barbol[1][1] == barbol[2][0])
 		{
-			if (arbol[0][2] == player) return +10;
-			else if (arbol[0][2] == opponent) return -10;
+			if (barbol[0][2] == player) return +10;
+			else if (barbol[0][2] == opponent) return -10;
 		}
 		
 		// Si no hay nada más se devuelve 0.
 		return 0;
 	}
+
+	// Función para elegir casillas libres para seleccionar el siguiente movimiento 
+	// de ficha y evaluarlo.
+	// Es invocado por minimax que evalua cada movimiento creando una foto distinta
+	// de arbol por cada movimiento nuevo que se evalua.
+	public Boolean isMove(String b[][]) {
+		for (int i = 0; i < fila; i++)
+		{
+			for (int j = 0; j < columna; j++)
+			{
+				if (b[i][j] == "-1") return true;
+				
+			}
+		}
+		return false;
+	}
 	
+	public int minimax(String board[][], int depth, Boolean isMax) {
+		int score = evaluate(board);
+		
+		if (score >= 10) return score;
+		if (score <= -10) return score;
+		if (isMove(board) == false) return 0; //No hay más movimientos.
+		
+		// El que mueve es humano (isMax = true).
+		if (isMax)
+		{
+			int best = -1000;
+			
+			// buscamos una celda libre.
+			for (int i = 0; i < fila; i++)
+			{
+				for (int j = 0; j < columna; j++)
+				{
+					
+				}
+			}
+		}
+		return score;
+				
+	}
 }
